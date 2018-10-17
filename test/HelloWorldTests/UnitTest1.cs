3f12ae6a383c2baaef40bf2b23e5aab32fa18eb6
josetr <37419832+josetr@users.noreply.github.com>
2018-10-17 14:20:34 +0100

    Add files

---
+++ test/HelloWorldTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿using Xunit;
+
+namespace HelloWorldTests
+{
+    public class UnitTest1
+    {
+        [VsFact(Version = "2017", RootSuffix = "RoslynDev")]
+        public void TestMethod1()
+        {
+        }
+    }
+}
